bd79a4964794ffcb3134c9be1df9cc9b3abd42eb
tekelia-powe <66314860+tekelia-powe@users.noreply.github.com>
2020-08-31 22:57:51 -0500

    Week5 Assignments

--- week5/ES6/es6.js
+++ week5/ES6/es6.js
@@ -1,4 +1,4 @@
-/* 
+
 const age = 101
 const name = "John"
 
@@ -23,18 +23,22 @@ function runforLoop(pets){
     console.log("man name: ", name)
     return petObjects
 
+    
 }
-runforLoop(["cat", "dog"]) */
+runforLoop(["cat", "dog"]) 
 
-/* const carrots = ["bright orange","ripe", "rotten"]
+const carrots = ["bright orange","ripe", "rotten"]
 
 mapVegtables = arr => {
 return arr.map(carrot => {
     return { type: "carrot", name: carrot}
 });
 
-} */
+}
 
+console.log(mapVegtables([carrots]));
+
+//Re-write this .filter() using an arrow function:
 const people = [
 
     {
@@ -55,9 +59,57 @@ const people = [
     }
 ]
 
-function filterForFriendly (arr) {
+filterForFriendly = arr => {
 
-    return arr.filter(function(person){
+    return arr.filter(person => {
         return person.friendly
-    })
+    });
 }
+
+console.log(filterForFriendly(people));
+//Re-write the following functions to be arrow functions:
+doMathSum = (a,b) => (a+b)
+
+produceProduct = (a,b) => a*b
+
+//Write a printString function that takes firstName, lastName, and age as parameters and returns a string like the following:
+printString = (firstName="Jane", lastName="Doe", age=100) => console.log (`Hi ${firstName} ${lastName}, how does it feel to be ${age}?`)
+
+printString("Kate", "Stark", 40)
+
+//Use the shorthand syntax to make the following filter take up one line
+const animal = [
+
+    {
+        type: "dog",
+        name: "theodore"
+    },
+    {
+        type: "cat",
+        name: "whiskers"
+    },
+    {
+        type: "pig",
+        name: "piglette"
+    },
+    {
+        type: "dog",
+        name: "sparky"
+    }
+
+    
+
+];
+
+function filterForDogs(arr){
+
+    
+    return arr.filter(animal => {animal.type === "dog" ? true : false})
+        }
+    
+
+filterForDogs(animal)
+
+printString2 = (Name, location) => console.log (`Hi ${Name}! \nWelcome to ${location}. \nI hope you enjoyed your stay. Please ask the president of ${location} if you need anything.`)
+
+printString2("Janice", "Hawaii")
